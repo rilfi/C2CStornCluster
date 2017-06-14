2918d83eed197d9ab62f581989cb8016fde30979
rilfi <rilfi@live.com>
2017-06-14 15:33:35 +0530

    performance added

--- src/main/java/mlNERTopology/C2CStormTopology.java
+++ src/main/java/mlNERTopology/C2CStormTopology.java
@@ -16,13 +16,23 @@ public class C2CStormTopology {
     TopologyBuilder builder = new TopologyBuilder();
 
     builder.setSpout("Title", new Title_rich_Spout(), 1);
+    builder.setBolt("tp", new Persist_title(),1 ).shuffleGrouping("Title");
 
-   builder.setBolt("State", new State_rich_Bolt(),1 ).shuffleGrouping("Title");
-    builder.setBolt("Model", new Model_NER_rich_Bolt(), 1).shuffleGrouping("State");
-    builder.setBolt("Group", new Group_rich_Bolt(), 1).shuffleGrouping("Model");
-    builder.setBolt("NER", new NER_rich_Bolt(), 1).shuffleGrouping("Group");
 
-    builder.setBolt("persist", new Persist_rich_Bolt(), 1).shuffleGrouping("NER");
+    builder.setBolt("State", new State_rich_Bolt(),1 ).shuffleGrouping("Title");
+    builder.setBolt("sp", new Persist_State(),1 ).shuffleGrouping("State");
+
+    builder.setBolt("Model", new Model_NER_rich_Bolt(), 1).shuffleGrouping("Title");
+    builder.setBolt("mp", new Persist_Model(),1 ).shuffleGrouping("Model");
+
+    builder.setBolt("Group", new Group_rich_Bolt(), 1).shuffleGrouping("Title");
+    builder.setBolt("gp", new Persist_Model(),1 ).shuffleGrouping("Group");
+
+    builder.setBolt("NER", new NER_rich_Bolt(), 1).shuffleGrouping("Title");
+    builder.setBolt("np", new Persist_NER(),1 ).shuffleGrouping("NER");
+
+
+    //builder.setBolt("persist", new Persist_rich_Bolt(), 1).shuffleGrouping("NER");
 
     Config config = new Config();
     config.setDebug(true);
