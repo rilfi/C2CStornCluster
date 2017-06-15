4df5bdabdd81f80a67002586a10d3a9169e2bf13
rilfi <rilfi@live.com>
2017-06-15 10:40:57 +0530

    group only

--- src/main/java/mlNERTopology/C2CStormTopology.java
+++ src/main/java/mlNERTopology/C2CStormTopology.java
@@ -16,6 +16,8 @@ public class C2CStormTopology {
     TopologyBuilder builder = new TopologyBuilder();
 
     builder.setSpout("Title", new Title_rich_Spout(), 1);
+    builder.setBolt("Group", new Group_rich_Bolt(), 1).shuffleGrouping("Title");
+    builder.setBolt("gp", new Persist_Model(),1 ).shuffleGrouping("Group");
   /*  builder.setBolt("tp", new Persist_title(),1 ).shuffleGrouping("Title");
 
 
@@ -26,10 +28,10 @@ public class C2CStormTopology {
     builder.setBolt("mp", new Persist_Model(),1 ).shuffleGrouping("Model");
 
     builder.setBolt("Group", new Group_rich_Bolt(), 1).shuffleGrouping("Title");
-    builder.setBolt("gp", new Persist_Model(),1 ).shuffleGrouping("Group");*/
+    builder.setBolt("gp", new Persist_Model(),1 ).shuffleGrouping("Group");
 
-    builder.setBolt("NER", new NER_rich_Bolt(), 1).shuffleGrouping("Title");
-    builder.setBolt("np", new Persist_NER(),1 ).shuffleGrouping("NER");
+   /* builder.setBolt("NER", new NER_rich_Bolt(), 1).shuffleGrouping("Title");
+    builder.setBolt("np", new Persist_NER(),1 ).shuffleGrouping("NER");*/
 
 
     //builder.setBolt("persist", new Persist_rich_Bolt(), 1).shuffleGrouping("NER");
